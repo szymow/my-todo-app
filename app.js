534e111df8c80d4e611854e66c8f39f52bdbeab0
szymow <szymow94@gmail.com>
2026-03-21 20:24:04 +0100

    poprawna edytcji zadań

--- app.js
+++ app.js
@@ -133,12 +133,27 @@ mongoose.connect(DB_URL)
   });
 
 // Aktualizacja zadania (np. zmiana statusu na wykonane)
-app.patch('/tasks/:id', async (req, res) => {
+app.patch('/tasks/:id', auth, async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['title', 'dueDate', 'priority', 'completed'];
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Nieprawidłowe pola do aktualizacji!' });
+    }
+
     try {
-        const updatedTask = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true });
-        res.json(updatedTask);
-    } catch (err) {
-        res.status(400).json({ error: "Nie udało się zaktualizować zadania" });
+        const task = await Task.findOne({ _id: req.params.id, owner: req.user._id });
+
+        if (!task) {
+            return res.status(404).send();
+        }
+
+        updates.forEach((update) => task[update] = req.body[update]);
+        await task.save();
+        res.send(task);
+    } catch (e) {
+        res.status(400).send(e);
     }
 });
 
